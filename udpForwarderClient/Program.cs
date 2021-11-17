328551e0746aed1f05add0e03dc349ac3efe5cf7
Casper Hougaard Jensen <chj@iterator-it.dk>
2021-11-17 22:23:15 +0100

    Client works with TCP

--- udpForwarderClient/Program.cs
+++ udpForwarderClient/Program.cs
@@ -11,16 +11,16 @@ namespace udpForwarderClient
 
         private static string telemetryClientIP;
         private static int telemetryClientPort;
-        private static string udpServerIP;
-        private static int udpServerPort;
+        private static string serverIP;
+        private static int serverPort;
 
         static void Main(string[] args)
         {
             telemetryClientIP = "127.0.0.1";
             telemetryClientPort = 9995;
 
-            udpServerIP = "127.0.0.1";
-            udpServerPort = 8881;
+            serverIP = "192.168.1.95";
+            serverPort = 8881;
 
             Task.Run(async () =>
             {
@@ -28,14 +28,7 @@ namespace udpForwarderClient
                 {
                     Console.WriteLine("Forwarder Client started!");
                     
-                    var client = new TcpClient(udpServerIP, udpServerPort);
-                    // client.Connect(udpServerIP, udpServerPort);
-                    // await client.ConnectAsync();
-                    
-                    // var ready = Encoding.ASCII.GetBytes("READY");
-
-                    // //Connect to Server and signal ready
-                    // await client.SendAsync(ready, ready.Length);
+                    var client = new TcpClient(serverIP, serverPort);
 
                     //Prepare to send data to telemetry software
                     var telemetryClient = new UdpClient();
@@ -45,19 +38,14 @@ namespace udpForwarderClient
 
                     while (true)
                     {
-                        
-
-                        // var packet = await client.ReceiveAsync();
-
                         var buffer = new byte[2024];
 
-                        stream.Read(buffer, 0, buffer.Length);
+                        var numbytes = stream.Read(buffer, 0, buffer.Length);
 
                         var packet = buffer;
+                        var sentbytes = await telemetryClient.SendAsync(packet, numbytes);
 
-                        await telemetryClient.SendAsync(packet, packet.Length);
-
-                        Console.WriteLine($"Received Packet, sized {packet.Length}");
+                        Console.WriteLine($"Send Packet, sized {numbytes}");
                     }
 
                 }
